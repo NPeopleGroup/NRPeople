fe8cacfbf98939c90f415d48a99bd28b9de05611
search <xiangyanglai@126.com>
2015-06-30 14:25:51 +0800

    1.整合后台管理页面 2.加入登陆过滤

--- NorthPeopleJob/src/com/hyd/northpj/action/PrintAction.java
+++ NorthPeopleJob/src/com/hyd/northpj/action/PrintAction.java
@@ -13,27 +13,9 @@ public class PrintAction extends ActionSupport{
 	
 	@Override
 	public String execute() throws Exception {
-		// TODO Auto-generated method stub
-	/*	System.out.println("******");
-		AdminUser adminUser1=new AdminUser();
-		adminUser1.setName("name");
-		adminUser1.setSn(1);
-		adminUser1.setPassword("password");
-		adminUser1.setType("type");
-		adminUser1.setUsername("userName");
-		AdminUser adminUser2=new AdminUser();
-		adminUser2.setName("name1");
-		adminUser2.setSn(2);
-		adminUser2.setPassword("password2");
-		adminUser2.setType("type2");
-		adminUser2.setUsername("userName2");
-		ArrayList<AdminUser> myAdminUserList=new ArrayList<AdminUser>();
-		myAdminUserList.add(adminUser1);
-		myAdminUserList.add(adminUser2);*/
 		AdminUserService myAdminUserService=new AdminUserService();
 		List<User> myUserList=myAdminUserService.getNormalUserList();
 		CommonUtil.toBeJson(myUserList, myUserList.size());
-//		System.out.println(myUserList.toString());
 		
 		
 		return null;
